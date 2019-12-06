6263d6cf95e5c7279a27e71429d9cd49853b8fc5
Tom Dalby <thomasjackdalby@gmail.com>
2019-12-06 07:15:29 +0000

    tweaks to reset

--- reset_database.py
+++ reset_database.py
@@ -5,18 +5,18 @@ from noise import random_gaussian
 import random
 
 with app.app_context():
-    #db.drop_all()
+    db.drop_all()
     db.create_all()
-    #db.session.commit()
 
     with open("gifts.csv", "r") as file:
         lines = file.readlines()
-        for line in lines:
-            item = Item(name=line.strip())
-            item.amount = random.randint(1, 10)
-            item.cost_min = int(random.random() * 50_00)
-            item.cost_range = int(random_gaussian(50, 50_00))
-            item.cost_seed = int(random.randint(0, 1000_00))
-            item.cost_offset = int(random.random() * 1000_00)
-            db.session.add(item)
-        db.session.commit()
+    
+    for line in lines:
+        item = Item(name=line.strip())
+        item.amount = random.randint(1, 10)
+        item.cost_min = int(random.random() * 50_00)
+        item.cost_range = int(random_gaussian(50, 50_00))
+        item.cost_seed = int(random.randint(0, 1000_00))
+        item.cost_offset = int(random.random() * 1000_00)
+        db.session.add(item)
+    db.session.commit()
