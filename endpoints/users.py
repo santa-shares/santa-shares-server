fecef94669d5976c024bc5e9cc6dac378633be2f
Tom Dalby <thomasjackdalby@gmail.com>
2019-12-24 11:54:21 +0000

    updated

--- endpoints/users.py
+++ endpoints/users.py
@@ -99,7 +99,7 @@ class User(Resource):
         if user is None: abort(400, "No user found with that id.")
 
         user_items = models.UserItem.query.filter_by(user_id=user_id).all()
-        user.user_items = user_items
+        user.items = user_items
         items = models.Item.query.all()
 
         item_lookup = {}
